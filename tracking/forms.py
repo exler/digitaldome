5baecd2370d9de0640406266444859ddc06c68f8
Kamil Marut <kamil@kamilmarut.com>
2023-10-30 21:59:36 +0100

    fix: Fix updating tracking via form

--- tracking/forms.py
+++ tracking/forms.py
@@ -5,7 +5,24 @@ from django import forms
 from tracking.models import TrackingObject
 
 
-class TrackingObjectForm(forms.ModelForm):
+class DynamicFieldFormMixin:
+    """
+    This mixin makes the form only update the fields that are available
+    in the supplied data.
+    """
+
+    def __init__(self: Self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+        fields_copy = self.fields.copy()
+        for field in self.fields:
+            if field not in self.data.keys():
+                del fields_copy[field]
+
+        self.fields = fields_copy
+
+
+class TrackingObjectForm(DynamicFieldFormMixin, forms.ModelForm):
     class Meta:
         model = TrackingObject
         fields: ClassVar = ["status", "rating", "notes"]
